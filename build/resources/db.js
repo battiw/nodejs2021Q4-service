fde73c5c4ba08b8295214ff000dc90c50f1700d9
Wit <battiwgood@gmail.com>
2021-12-21 22:49:45 +0300

    feat: eslint config

--- build/resources/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dataArrayDB = exports.dataArrayTasksDB = exports.dataArrayBoardDB = void 0;
-const dataArrayBoardDB = [];
-exports.dataArrayBoardDB = dataArrayBoardDB;
-const dataArrayTasksDB = [];
-exports.dataArrayTasksDB = dataArrayTasksDB;
-const dataArrayDB = [];
-exports.dataArrayDB = dataArrayDB;
