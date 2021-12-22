1be4f9c47aba9d18b4ede330d825b555f7d1d321
Wit <battiwgood@gmail.com>
2021-12-22 15:38:34 +0300

    feat: logger simple and looger Morgan

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,12 @@ import SwaggerUI from 'swagger-ui-express';
 import pathToSwagg from 'path';
 import YAML from 'yamljs';
 
+// import { logger } from './log/logger'; //simple logging
+// import { finished } from 'stream'; // simple logging
+// import * as fs from 'fs'; // simple logging
+import morgan from 'morgan'; // Morgan logging
+import { createWriteStream } from 'fs'; // Morgan loggin
+
 import { routerUser } from './resources/users/user.router';
 import { boardRouter } from './resources/board/board.router';
 
@@ -12,6 +18,22 @@ const swaggerDocument = YAML.load(
 );
 
 app.use(Express.json());
+
+// app.use(logger); // simple logging
+
+app.use(
+  morgan('combined', {
+    stream: createWriteStream('./src/log/loggerMorgan.log'),
+  })
+); // Morgan logging all
+app.use(
+  morgan('combined', {
+    skip: function (_req, res) {
+      return res.statusCode < 400;
+    },
+    stream: createWriteStream('./src/log/loggerMorganError.log'),
+  })
+); // Morgan logging error
 
 app.use('/doc', SwaggerUI.serve, SwaggerUI.setup(swaggerDocument));
 
