4da72ae5a300b67ab077e4c99f6bd443defe75e0
Wit <battiwgood@gmail.com>
2022-01-09 18:09:00 +0300

    fix:restart ver

--- src/resources/users/user.router.ts
+++ src/resources/users/user.router.ts
@@ -17,7 +17,7 @@ routerUser.route('/').get(async (_req: Request, res: Response) => {
    * Function returns all users
    * @returns Promis function call result getAllServis
    */
-  console.log('HEELLLLOOOOO&&(((((((((((((((((((DIMA777)))))))))))))))))))');
+  console.log('HEELLLLOOOOO&&(((((((((((((((((((DIMA77745)))))))))))))))))))');
 
   const users = await getAllServis();
   res.json(users.map(User.toResponse));
